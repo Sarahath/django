2bb0f23652334a23064be5d8038723b042d88bb9
umar <sarajhathumar207@gmail.com>
2026-05-01 20:30:31 +0530

    rev

--- ind/settings.py
+++ ind/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 from urllib.parse import urlparse,parse_qsl
 import os
 from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
